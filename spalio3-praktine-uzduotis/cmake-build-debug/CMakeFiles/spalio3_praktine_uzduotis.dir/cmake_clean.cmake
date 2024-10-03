bef947404025b430821929d806148ccd10eac6e6
Ksenija Perechrest <s056857@ad.viko.lt>
2024-10-03 10:45:28 +0300

    pabaigtas darbas

---
+++ spalio3-praktine-uzduotis/cmake-build-debug/CMakeFiles/spalio3_praktine_uzduotis.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spalio3_praktine_uzduotis.dir/main.cpp.obj"
+  "CMakeFiles/spalio3_praktine_uzduotis.dir/main.cpp.obj.d"
+  "libspalio3_praktine_uzduotis.dll.a"
+  "spalio3_praktine_uzduotis.exe"
+  "spalio3_praktine_uzduotis.exe.manifest"
+  "spalio3_praktine_uzduotis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spalio3_praktine_uzduotis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

6d2f4d7e3be6aa99f96b1cf083fd9e3bbc343fe0
Ksenija Perechrest <s056857@ad.viko.lt>
2024-10-03 11:22:30 +0300

    Pradetas darbas

---
+++ spal3-4/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+//----------------------------pirmas pratimas------------------------------
+int suskaiciuotiPerimetra(int krastine) {
+    int krastine = 10;
+    cout<<"Funkcijai perduodama reiksme = "<< krastine << endl;
+
+
+    return 0;
+}
+
+int suskaiciuotiPlota(int krastine, int&plotas) {
+
+    return 0;
+}
+
+int main()
+{
+
+    return 0;
+}

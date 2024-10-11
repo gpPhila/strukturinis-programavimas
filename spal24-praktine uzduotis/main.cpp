c72399623d1c6b6c7540fe3bb1ee60cb0f7e240e
Ksenija Perechrest <s056857@ad.viko.lt>
2024-10-11 09:53:31 +0300

    padariau antra dali

--- spal24-praktine uzduotis/main.cpp
+++ spal24-praktine uzduotis/main.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 bool balsisArNe (char raide); //prototipas
+int didBendrasDaliklis (int skaicius1, int skaicius2); //prototipas
+void zaidimas (); //prototipas
 
 int main()
 {
     int operacija=0;
     char raide;
-    int rezultatas1;
+    string rezultatas1;
+    int skaicius1, skaicius2;
 
     while (operacija !=5) {
         cout<<"Pasirinkite operacija, ivesdami operacijos numeri: "<<endl;
         cout<<"1. Pasitikrinti ar ivesta raide yra balsis."<<endl;
-        cout<<"2. "<<endl;
-        cout<<"3. "<<endl;
+        cout<<"2. Rasti didziausia bendra dalikli taip dvieju skaitmenu."<<endl;
+        cout<<"3. Zaidimas: atspekite attsitiktini skaiciu."<<endl;
         cout<<"4. "<<endl;
         cout<<"5. Iseiti is programos."<<endl;
         cin>>operacija;
@@ -22,12 +25,15 @@ int main()
         case 1:
             cout<<"Iveskite raide: "<<endl;
             cin>>raide;
-
-            cout<<balsisArNe(raide)<<endl;
-            rezultatas1 == (balsisArNe(raide)) ? "Raide yra balsis." : "Raide nera balsis.";
-            
+            rezultatas1 = (balsisArNe(raide)) ? "Raide yra balsis." : "Raide nera balsis.";
+            cout<<rezultatas1<<endl;
             break;
         case 2:
+            cout<<"Iveskite du skaicius."<<endl;
+            cin>>skaicius1>>skaicius2;
+            didBendrasDaliklis(skaicius1, skaicius2);
+            cout<<"Maziausias bendras daliklis tarp "<<skaicius1<<" ir "<<skaicius2<<" yra "<<
+                didBendrasDaliklis(skaicius1, skaicius2)<<"."<<endl;
             break;
         case 3:
             break;
@@ -52,4 +58,24 @@ bool balsisArNe(char raide) {
     } else {
         return false;
     }
+}
+
+int didBendrasDaliklis(int skaicius1, int skaicius2) {
+
+    while (skaicius2 !=0 ) {
+        int skaicius3 = skaicius1 % skaicius2;
+        skaicius1 = skaicius2;
+        skaicius2 = skaicius3;
+    }
+    return skaicius1;
+}
+//(liekana sk3=) 4 = 36 % 16
+//sk1 = 16
+//sk2 = 4
+//liekana 0 = 16 % 4
+// sk1 = 4, sk2 = 0 => returns sk1 which is 4
+
+void zaidimas () {
+
+    
 }

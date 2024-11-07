0a09d4e37d983a1608caff1228a0718a383e0fbf
Phila Novak <ksenija.perechrest@gmail.com>
2024-11-08 00:12:20 +0200

    checking

---
+++ spal17/cmake-build-debug/CMakeFiles/spal17.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spal17.dir/main.cpp.obj"
+  "CMakeFiles/spal17.dir/main.cpp.obj.d"
+  "libspal17.dll.a"
+  "spal17.exe"
+  "spal17.exe.manifest"
+  "spal17.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spal17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

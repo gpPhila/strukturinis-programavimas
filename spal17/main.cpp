1168637d4b08cbabe3b7d1351f2f4630fb40b0ff
Ksenija Perechrest <s056857@ad.viko.lt>
2024-11-08 08:48:15 +0200

    pataisytas

--- spal17/main.cpp
+++ spal17/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
 
-    const int eilutes=5;
+    const int eilutes=3;
     const int stulpeliai=3;
     int masyvas [eilutes][stulpeliai];
     int i = 0;
 
-    //ivedimas 5x3 dydzio masyvo, ivedziame reiksmes i masyva
+    //ivedimas 3x3 dydzio masyvo, ivedziame reiksmes i masyva
 
-    cout<<"Iveskite 5iu eiluciu 3 stulpeliu masyvo elementus"<<endl;
+    cout<<"Iveskite 3iu eiluciu 3 stulpeliu masyvo elementus"<<endl;
     for (int i=0; i < eilutes; i++) {
         for (int j=0; j < stulpeliai; j++) {
             cout<<"Masyvo ["<<i+1<<"]["<<j+1<<"] elementas"<<endl;
@@ -23,7 +23,7 @@ int main()
     cout<<"Masyvo spausdinimas"<<endl;
     for (int i=0; i < eilutes; i++) {
         for (int j=0; j < stulpeliai; j++) {
-            cout<<setw(5)<<masyvas [i][j]<<" "<<endl;
+            cout<<setw(3)<<masyvas [i][j]<<" "<<endl;
         }
         //kad kiekviena eilute butu is naujos
         cout << endl;
@@ -32,22 +32,22 @@ int main()
     for (int i=0; i < eilutes; i++) {
         int eilutesSuma = 0;
         for (int j=0; j < stulpeliai; j++) {
-            cout<<setw(5)<<masyvas [i][j]<<" "<<endl;
+            cout<<setw(3)<<masyvas [i][j]<<" "<<endl;
             eilutesSuma += masyvas[i][j];
         }
         //kad kiekviena eilute butu is naujos
-        cout<<setw(5)<<eilutesSuma;
+        cout<<setw(3)<<eilutesSuma;
         cout << endl;
     }
     cout<<"Masyvo spausdinimas ir stulpelio elementu suma"<<endl;
     for (int j=0; j<stulpeliai; j++) {
         int stulpelioSuma = 0;
         for (i=0; i<eilutes; i++) {
-            cout<<setw(5)<<masyvas [i][j]<<" "<<endl;
+            cout<<setw(3)<<masyvas [i][j]<<" "<<endl;
             stulpelioSuma += masyvas[i][j];
         }
         cout << endl;
-        cout<<setw(5)<<"Stulpelio elementu suma"<<stulpelioSuma<<" "<<endl;
+        cout<<setw(3)<<"Stulpelio elementu suma"<<stulpelioSuma<<" "<<endl;
 
     }
     return 0;

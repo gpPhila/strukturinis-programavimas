e3344410c73011fe3ec960f77b266d78d86ad55b
Phila Novak <ksenija.perechrest@gmail.com>
2024-11-19 23:41:37 +0200

    another meaningless commit meh

--- lap21-praktine-uzduotis/main.cpp
+++ lap21-praktine-uzduotis/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <string.h>
 using namespace std;
 
-const char ABECELE[32] = {'A', 'Ą', 'B', 'C', 'Č', 'D', 'E', 'Ę', 'Ė', 'F', 'G', 'H',
-                          'I', 'Į', 'Y', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S',
-                          'Š', 'T', 'U', 'Ų', 'Ū', 'V', 'Z', 'Ž'};
-void sifravimas1();
+const char ABECELE[23] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                          'I', 'Y', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S',
+                          'T', 'U', 'V', 'Z'};
+void sifravimas1();//prototype
+void desifravimas1();
+void sifravimas2();//prot
+void desifravimas2();
+
 
 int main()
 {
@@ -18,9 +24,11 @@ int main()
         cin>>pasirinkimas;
 
         switch (pasirinkimas) {
-            case 1: cout<<"Pasirinkimas 1"<<endl;
+            case 1: cout<<"Pasirinkimas 1:"<<endl;
+                    sifravimas1();
             break;
-            case 2: cout<<"Pasirinkimas 2"<<endl;
+            case 2: cout<<"Pasirinkimas 2:"<<endl;
+                    sifravimas2();
             break;
             case 3: cout<<"Jus isejote. Viso gero."<<endl;
             break;
@@ -33,7 +41,40 @@ int main()
 }
 
 void sifravimas1() {
+    char tekstas1[50];
+    char raktas1 [50];
+    char uzsifruotasTekstas1[50];
+
+    int abecelesDydis = sizeof(ABECELE)/sizeof(ABECELE[0]);
+
+    cout<<"Iveskite teksta: "<<endl;
+    cin>>tekstas1;
+    cout<<"Iveskite slapta rakta: "<<endl;
+    cin>>raktas1;
+    int tekstoIlgis = strlen(tekstas1);
+    int raktoIlgis = strlen(raktas1);
+    cout<<tekstoIlgis<<" "<<abecelesDydis<<endl;
+
+    for (int i=0; i < tekstoIlgis; i++) {
+    }
+}
+
+void sifravimas2() {
+    char tekstas2[50];
+    char raktas2[50];
+    char uzsifruotasTekstas[50];
+
+    cout << "Iveskite teksta: "<<endl;
+    cin >> tekstas2;
+    cout << "Iveskite slapta rakta: "<<endl;
+    cin >> raktas2;
+
+    int tekstoIlgis = strlen(tekstas2);
+    int raktoIlgis = strlen(raktas2);
+
+    for (int i = 0; i < tekstoIlgis; ++i) {
 
 
 
+    }
 }

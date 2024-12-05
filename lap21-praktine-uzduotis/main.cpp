5fa989462bf5d419f1642d7d34330258f4420dca
Ksenija Perechrest <s056857@ad.viko.lt>
2024-12-05 11:48:30 +0200

    nieko naujo

--- lap21-praktine-uzduotis/main.cpp
+++ lap21-praktine-uzduotis/main.cpp
@@ -217,6 +217,7 @@ void desifravimas2() {
         // jeigu i=3 o raktas2 turi tik 2 simbolius, tai 3%2 => grizta prie pirmo simbolio, naudoja ji tolimesniam skaiciavimui
 
         rezultatas[i] = (tekstoASCII - raktoASCII + 94) % 94 + 33 + 28;
+        //rezultatas[i] = (tekstoASCII - raktoASCII + 94) % 94 + 33;
         char result = getOutOfASCII(rezultatas[i]);
         cout << result;
     }

c349a344b7e8a6ef87b12fbc102edaf2bd218c96
Ksenija Perechrest <s056857@ad.viko.lt>
2025-01-09 11:15:39 +0200

    patikrintas darbas

--- saus9-penktoji-praktine-uzduotis/cmake-build-debug/CMakeFiles/saus9_penktoji_praktine_uzduotis.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for saus9_penktoji_praktine_uzduotis.
--- saus9-penktoji-praktine-uzduotis/cmake-build-debug/CMakeFiles/saus9_penktoji_praktine_uzduotis.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for saus9_penktoji_praktine_uzduotis.
-# This may be replaced when dependencies are built.
